7a3406df8c777ae3fbd4595708b79ed2e6e706ef
daphtya <davidpanjaitan1@gmail.com>
2017-11-18 17:52:06 +0700

    command recruit dengan asumsi adt player sudah jadi

--- command.c
+++ command.c
@@ -45,11 +45,53 @@ void move(Unit U)
 
 void undo() /* adam fadhel */
 void change_unit()
-void recruit()
-{
-  if (strcmp(TypeName(U),"King") == 0)
-  {
 
+boolean can_call_recruit_comm() {
+	boolean temp, adakosong;
+	POINT P = Position(*SelectedUnit);
+	temp = strcmp(TypeName(*SelectedUnit),"King") == 0 && PlotType(Petak(M, P)) == 'T';
+	adakosong = false;
+	Geser(&P, 0, 1);
+	adakosong ||= IsPlotEmpty(M,P);
+	Geser(&P, 0, -2);
+	adakosong ||= IsPlotEmpty(M,P);
+	Geser(&P, 1, 1);
+	adakosong ||= IsPlotEmpty(M,P);
+	Geser(&P, -2, 0);
+	adakosong ||= IsPlotEmpty(M,P);
+
+	return temp && adakosong;
+}
+
+void recruit()   // asumsi: ada global variabel Unit* SelectedUnit dan Player* currPlayer, ada primitif AddUnit di ADT player
+{
+  if (can_call_recruit_comm())
+  {
+	POINT P;
+	int i;
+	
+	do {
+		printf("Enter the coordinate for recruitment : ");
+		scanf("%d %d", &Absis(P), &Ordinat(P));
+		if (!(IsPlotEmpty(M, P) && PlotType(Petak(M,P)) == 'C')) {
+			printf("That castle is occupied!\n");
+		}
+	}while (!(IsPlotEmpty(M, P) && PlotType(Petak(M,P)) == 'C'));
+	printf("========== List of recruitable units ==========\n");
+	for (i = 1; i<NbUnitType; i++) {
+		printf("%d. %s | Health %d | ATK %d | Cost %dG\n", i, TypeList[i].TypeName, TypeList[i].MaxHP, TypeList[i].Atk, TypeList[i].Cost);
+	}
+	printf("Enter the index of the unit you want to recruit :");
+	scanf("%d", &i);
+	if (Gold(*currPlayer) >= TypeList[i].Cost) {
+		Gold(*currPlayer) -= TypeList[i].Cost;
+		AddUnit(CreateUnit(i, Absis(P), Ordinat(P)), *currPlayer);
+	} else {
+		printf("You don't have enough gold to recruit that unit!\n");
+	}
+	
+  } else {
+	  printf("Only King on a Tower plot with at least one adjacent empty castle can recruit more units!\n");
   }
 }
 void attack();
